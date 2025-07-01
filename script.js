87545e8d50a2cbbd4be95557a2315872e1949a67
power hub <82532313+Kellyovita@users.noreply.github.com>
2025-07-01 10:23:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -208,3 +208,10 @@ function getGreetingAndStatus() {
 
 // Run greeting on page load
 document.addEventListener("DOMContentLoaded", getGreetingAndStatus);
+const menuToggle = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
